374bda415f22e1d52c8a7cd19477b789e3cf3917
Mackram Raydan <mackram@imagimate.com>
2020-01-18 00:19:22 +0200

    fixed congraph typos as well as supporting named parameters for the generated function

--- src/congraph/core.clj
+++ src/congraph/core.clj
@@ -1,13 +1,14 @@
 (ns congraph.core
   (:require [clojure.java.io :as io]
             [clojure.set :refer [rename-keys]]
-            [hugneo4j.core :as hugneo4j])
+            [hugneo4j.core :as hugneo4j]
+            [hugneo4j.cypher :as cypher])
   (:import (clojure.lang IDeref)))
 
 (defn validate-files [filenames]
   (doseq [file filenames]
     (when-not (or (instance? java.io.File file) (io/resource file))
-      (throw (Exception. (str "conman could not find the query file:" file))))))
+      (throw (Exception. (str "congraph could not find the query file:" file))))))
 
 (defn try-snip [[id snip]]
   [id
@@ -47,16 +48,40 @@
    filenames))
 
 (defn intern-fn [ns id meta f]
-  (intern ns (with-meta (symbol (name id)) meta) f))
+  (intern ns (with-meta
+               (symbol (name id))
+               (-> meta
+                   (assoc :arglists
+                          (if (empty? (:keys (second (first (:arglists meta)))))
+                            `([] [~'conn & ~'args])
+                            `([~(second (first (:arglists meta)))]
+                              [~'conn ~(second (first (:arglists meta))) & ~'args]))))) f))
 
 (defmacro bind-connection [conn & filenames]
   `(let [{snips# :snips fns# :fns :as queries#} (congraph.core/load-queries ~@filenames)]
      (doseq [[id# {fn# :fn meta# :meta}] snips#]
-       (conman.core/intern-fn *ns* id# meta# fn#))
+       (congraph.core/intern-fn *ns* id# meta# fn#))
      (doseq [[id# {query# :fn meta# :meta}] fns#]
-       (conman.core/intern-fn *ns* id# meta#
+       (congraph.core/intern-fn *ns* id# meta#
                               (fn f#
                                 ([] (query# ~conn {}))
                                 ([params#] (query# ~conn params#))
                                 ([conn# params# & args#] (apply query# conn# params# args#)))))
      queries#))
+
+(defn connect
+  ([bolt-url] (connect bolt-url nil nil))
+  ([bolt-url username] (connect bolt-url username nil))
+  ([bolt-url username password]
+   (cypher/connect bolt-url username password)))
+
+(defn disconnect
+  [conn]
+  (.close conn))
+
+(defn set-audit-defaults
+  [& {:keys [node-label node-id-attribute
+             default-node-id audit-relationship
+             audit-node audit-made-relationship]
+      :as audit-params}]
+  (cypher/set-audit-params audit-params))
