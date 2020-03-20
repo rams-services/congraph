2fa18b4c29b4fdbb21b042db38fa34d076733703
Mackram Raydan <mackram@imagimate.com>
2020-03-20 15:02:32 +0200

    bumped version of hugneo4j

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject congraph "0.0.6"
+(defproject congraph "0.0.7"
   :description "a neo4j connection management library based on conman"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [hugneo4j "0.0.6"]]
+                 [hugneo4j "0.0.7"]]
   :profiles
   {:dev
    {:dependencies [[mount "0.1.16"]]}})
