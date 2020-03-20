47696d24628562852ce62d5556a42c7bf8f1060c
Mackram Raydan <mackram@imagimate.com>
2020-03-20 13:57:43 +0200

    added support for the hugneo4j version that handles collections

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject congraph "0.0.5"
+(defproject congraph "0.0.6"
   :description "a neo4j connection management library based on conman"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [hugneo4j "0.0.5"]]
+                 [hugneo4j "0.0.6"]]
   :profiles
   {:dev
    {:dependencies [[mount "0.1.16"]]}})
