a37b9676355252bb89cbd00f8241b4f19739c807
Hassan <hassan.ibrahim.96@outlook.com>
2020-11-24 16:15:58 +0200

    Bumped hugneo4j version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [hugneo4j "0.0.9"]]
+                 [hugneo4j "0.1.0"]]
   :profiles
   {:dev
    {:dependencies [[mount "0.1.16"]]}})
